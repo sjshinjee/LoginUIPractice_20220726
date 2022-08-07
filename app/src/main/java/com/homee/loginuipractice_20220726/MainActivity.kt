3465b7bc325a95d49b5e8e1aae24697ebb8bef70
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-08-08 00:11:56 +0900

    기초 이벤트 처리(click listener 활용) -toast, log

--- app/src/main/java/com/homee/loginuipractice_20220726/MainActivity.kt
+++ app/src/main/java/com/homee/loginuipractice_20220726/MainActivity.kt
@@ -2,10 +2,26 @@ package com.homee.loginuipractice_20220726
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailTxt.text = "아이디 아니다"
+
+        //기초 이벤트 처리(setOnClickListener > 클릭 이벤트 처리)
+
+        signUpBtn.setOnClickListener {
+            //버튼이 눌릴때마다 실행시켜줄 코드를 이 중괄호 안에 작성
+            Toast.makeText(this, "회원가입 버튼 클릭", Toast.LENGTH_SHORT).show()
+        }
+
+        loginBtn.setOnClickListener {
+            Log.d("로그인 버튼","클릭")
+        }
     }
 }
